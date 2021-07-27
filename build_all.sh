316a163f92ac32032843c902829cd8958f462327
dixyes <dixyes@gmail.com>
2021-07-27 16:54:07 +0800

    Fix build_all.sh routines

--- build_all.sh
+++ build_all.sh
@@ -2,21 +2,32 @@
 
 # simply loop to build all supported versions
 
-BUILD_IMAGE="${BUILD_IMAGE-${workdir}/build.sh}"
-
 mian()
 {
+    local target="$1"
     # since we are using bash, there's array!
     local SUPPORTED_ALPINE=(edge 3.14 3.13 3.12 3.11 3.10)
+    #local SUPPORTED_ALPINE=(3.12)
     local SUPPORTED_PHP=(8.0 7.4 7.3)
+    #local SUPPORTED_PHP=(7.4)
 
     local workdir
-    workdir="$(basedir "${BASH_SOURCE[0]}")"
-    for alpinever in "${SUPPORTED_ALPINE[@]}"
+    workdir="$(dirname "${BASH_SOURCE[0]}")"
+    local BUILD_IMAGE="${BUILD_IMAGE-${workdir}/build.sh}"
+
+    local alpinever; for alpinever in "${SUPPORTED_ALPINE[@]}"
     do
-        for phpver in "${SUPPORTED_PHP[@]}"
+        local phpver; for phpver in "${SUPPORTED_PHP[@]}"
         do
-            $BUILD_IMAGE alpine/base "ALPINE_VERSION=${alpinever##v}" "PHP_VERSION=${phpver}"
+            case $target in
+                *) ;&
+                "base")
+                    $BUILD_IMAGE alpine/base "ALPINE_VERSION=${alpinever##v}" "PHP_VERSION=${phpver}" ;;
+                "swoole")
+                    $BUILD_IMAGE alpine/swoole "ALPINE_VERSION=${alpinever##v}" "PHP_VERSION=${phpver}" SWOOLE_VERSION="${SWOOLE_VERSION-v4.7.0}" ;;
+                "swow")
+                    $BUILD_IMAGE alpine/swow "ALPINE_VERSION=${alpinever##v}" "PHP_VERSION=${phpver}" SWOW_VERSION="${SWOW_VERSION-v0.1.0-nightly20210601}" ;;
+            esac
         done
     done
 }
