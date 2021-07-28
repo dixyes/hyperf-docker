5ed169a1d3d8d60bf2cb83290f60f7019058b6bb
dixyes <dixyes@gmail.com>
2021-07-28 15:39:00 +0800

    Implement github workflows

--- build_all.sh
+++ build_all.sh
@@ -6,9 +6,12 @@ mian()
 {
     local target="$1"
     # since we are using bash, there's array!
+    # also, these arrays is used for actions to generate tasks
     local SUPPORTED_ALPINE=(edge 3.14 3.13 3.12 3.11 3.10)
     #local SUPPORTED_ALPINE=(3.12)
     local SUPPORTED_PHP=(8.0 7.4 7.3)
+    local SUPPORTED_SWOOLE=(master v4.7.0)
+    local SUPPORTED_SWOW=(develop v0.1.0-nightly20210601)
     #local SUPPORTED_PHP=(7.4)
 
     local workdir
@@ -24,9 +27,25 @@ mian()
                 "base")
                     $BUILD_IMAGE alpine/base "ALPINE_VERSION=${alpinever##v}" "PHP_VERSION=${phpver}" ;;
                 "swoole")
-                    $BUILD_IMAGE alpine/swoole "ALPINE_VERSION=${alpinever##v}" "PHP_VERSION=${phpver}" SWOOLE_VERSION="${SWOOLE_VERSION-v4.7.0}" ;;
+                    if [ "${SWOOLE_VERSION}" = "" ]
+                    then
+                        local swver; for swver in "${SUPPORTED_SWOOLE[@]}"
+                        do
+                            $BUILD_IMAGE alpine/swoole "ALPINE_VERSION=${alpinever##v}" "PHP_VERSION=${phpver}" SWOOLE_VERSION="${swver}"
+                        done
+                    else
+                        $BUILD_IMAGE alpine/swoole "ALPINE_VERSION=${alpinever##v}" "PHP_VERSION=${phpver}" SWOOLE_VERSION="${SWOOLE_VERSION}"
+                    fi ;;
                 "swow")
-                    $BUILD_IMAGE alpine/swow "ALPINE_VERSION=${alpinever##v}" "PHP_VERSION=${phpver}" SWOW_VERSION="${SWOW_VERSION-v0.1.0-nightly20210601}" ;;
+                    if [ "${SWOW_VERSION}" = "" ]
+                    then
+                        local swver; for swver in "${SUPPORTED_SWOW[@]}"
+                        do
+                            $BUILD_IMAGE alpine/swoole "ALPINE_VERSION=${alpinever##v}" "PHP_VERSION=${phpver}" SWOOLE_VERSION="${swver}"
+                        done
+                    else
+                        $BUILD_IMAGE alpine/swoole "ALPINE_VERSION=${alpinever##v}" "PHP_VERSION=${phpver}" SWOOLE_VERSION="${SWOW_VERSION}"
+                    fi ;;
             esac
         done
     done
